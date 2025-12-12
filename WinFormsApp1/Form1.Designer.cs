613351963161536d140a87367f522cc630b2f737
diegocardonahernandez <diego.cardona@tajamar365.com>
2025-12-12 10:25:17 +0100

    Commit

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,18 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnPulsar = new Button();
+            lblTitulo = new Label();
+            txtCaja = new TextBox();
             SuspendLayout();
+            // 
+            // btnPulsar
+            // 
+            btnPulsar.Location = new Point(330, 92);
+            btnPulsar.Name = "btnPulsar";
+            btnPulsar.Size = new Size(138, 47);
+            btnPulsar.TabIndex = 0;
+            btnPulsar.Text = "Pulsar";
+            btnPulsar.UseVisualStyleBackColor = true;
+            btnPulsar.Click += btnPulsar_Click;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Location = new Point(345, 25);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(123, 15);
+            lblTitulo.TabIndex = 1;
+            lblTitulo.Text = "Intrpduzca su nombre";
+            // 
+            // txtCaja
+            // 
+            txtCaja.BackColor = Color.White;
+            txtCaja.Location = new Point(294, 43);
+            txtCaja.Name = "txtCaja";
+            txtCaja.Size = new Size(231, 23);
+            txtCaja.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Brown;
             ClientSize = new Size(800, 456);
+            Controls.Add(txtCaja);
+            Controls.Add(lblTitulo);
+            Controls.Add(btnPulsar);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnPulsar;
+        private Label lblTitulo;
+        private TextBox txtCaja;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,5 +6,15 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {
+            //SOLO ESCRIBIMOS AQUI
+            this.txtCaja.Location = new Point(10, 30);
+            this.txtCaja.Text = "Soy un texto nuevo!!!";
+            this.txtCaja.Width = 220;
+            this.txtCaja.BackColor = Color.Fuchsia;
+        }
+
     }
 }
